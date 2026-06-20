1c48345e08a1bec5b175c73a356c06148e92951d
kads9905 <kadambari9905@gmail.com>
2026-06-20 10:28:30 +0530

    Implement snake rendering on game board

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const blockWidth = 50;
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
+console.log(rows, cols);
+console.log(board.clientHeight, board.clientWidth);
+
+const blocks = [];
+
+let snake = [
+    {
+        x: 0,
+        y: 3
+    }
+];
+
 for(let row = 0; row < rows; row++){
     for(let col = 0; col < cols; col++){
 
@@ -14,5 +26,14 @@ for(let row = 0; row < rows; row++){
         block.classList.add('block');
 
         board.appendChild(block);
+
+        blocks[`${row}-${col}`] = block;
     }
 }
+
+snake.forEach(segment => {
+    blocks[`${segment.x}-${segment.y}`]
+        .classList.add('fill');
+});
+
+
