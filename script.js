09fd2f76640e8c6d9a48baac76f186b0833661d4
kads9905 <kadambari9905@gmail.com>
2026-06-20 10:16:07 +0530

    Generate game grid dynamically using JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Snake Game");
+const board = document.querySelector('.board');
+
+const blockHeight = 50;
+const blockWidth = 50;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+
+for(let row = 0; row < rows; row++){
+    for(let col = 0; col < cols; col++){
+
+        const block = document.createElement('div');
+
+        block.classList.add('block');
+
+        board.appendChild(block);
+    }
+}
